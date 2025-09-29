3a3856d93fb128db9758fd0a47cbf7d7ba31496e
EmiGeimer6 <emicerg6@gmail.com>
2025-09-28 18:07:06 -0600

    Umis_CasaFinalCompletiaTeloComesKK_Version1

--- Assets/Project/Scripts/ECG/Item/Tape.cs
+++ Assets/Project/Scripts/ECG/Item/Tape.cs
@@ -2,14 +2,33 @@ using UnityEngine;
 
 public class Tape : Item, IUsable
 {
+
+    [SerializeField] private AudioData clothUseAudio;
     public void Use(GameObject user)
     {
-        Debug.Log("Usando");
+
+        AudioPlayer audioPlayer = FindAnyObjectByType<AudioPlayer>();
+
+        if (audioPlayer != null && clothUseAudio != null)
+        {
+            audioPlayer.Play(clothUseAudio);
+        }
+        else
+        {
+            Debug.Log($"No se pudo reproducir el audio {clothUseAudio} en {audioPlayer} ");
+        }
 
         RitualManager ritualManager = FindAnyObjectByType<RitualManager>();
-        if (ritualManager != null)
+        if (ritualManager != null && clothUseAudio != null)
         {
-            ritualManager.MarkUsedItemStepComplete();
+            ritualManager.MarkUsedItemStepComplete(); // Esto podrías dejarlo en AddRitualPoints
+
+        }
+        // El inventario elimina el objeto de sal
+        Inventory inventory = user.GetComponent<Inventory>();
+        if (inventory != null)
+        {
+            inventory.DeleteItem(this.gameObject);
         }
     }
 }
