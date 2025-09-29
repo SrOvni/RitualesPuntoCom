311310f6dab8256593854d748c43a04f11eccfb3
EmiGeimer6 <emicerg6@gmail.com>
2025-09-28 23:07:28 -0600

    Umiliano

--- Assets/Project/Scripts/ECG/Fusibles.cs
+++ Assets/Project/Scripts/ECG/Fusibles.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 public class Fusibles : MonoBehaviour, IInteractable
 {
     [SerializeField] private string interactText = "Fusibles";
+    [SerializeField] private AudioData interactSound;
     public string GetInteractText()
     {
         return interactText;
     }
     public void Interact(GameObject interactor)
     {
-        LightManager.Instance.TurnOnLights();
+        if (LightManager.Instance != null)
+        {
+            AudioPlayer audioPlayer = FindAnyObjectByType<AudioPlayer>();
+
+            if (audioPlayer != null && interactSound != null)
+            {
+                audioPlayer.Play(interactSound);
+            }
+            else
+            {
+                Debug.Log($"No se pudo reproducir el audio {interactSound} en {audioPlayer} ");
+            }
+
+            LightManager.Instance.ToggleLights();
+        }
     }
 }
